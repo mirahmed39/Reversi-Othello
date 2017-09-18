0826a7029ff748b8956aa94a3820b8193825e2c6
Mir Ahmed <mir.ahmed39@yahoo.com>
2017-09-18 11:15:59 -0400

    test helper function with values

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 // app.js
 
 let app = require('./reversi.js');
-const board = app.generateBoard(5, 5, " ");
+let x = 'something'; board = app.generateBoard(3, 3, " ");
 
 /*
 let str = "     A   B   C  \n   +---+---+---+\n 1 |   |   | O |\n   +---+---+---+\n 2 |   | X |   |\n   +---+---+---+\n 3 |   |   |   |\n   +---+---+---+\n";
 console.log(str);
 */
 //app.getCellsToFlip(board, 1, 4);
-console.log(app.getLowerLeftIndices(board, 4, 4, 5));
+board = app.placeLetters(board, 'O','A2');
+board = app.placeLetters(board, 'X', 'A1');
+console.log(app.getValidMoves(board, 'X'));
+
