eff52228ee4c66ee831bbcc86faec22181e2d8fe
mirahmed39 <mir.ahmed39@yahoo.com>
2017-09-19 15:13:05 -0400

    test getValidMoves function with different params

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 // app.js
 
 let app = require('./reversi.js');
-let x = 'something'; board = app.generateBoard(3, 3, " ");
+let board = app.generateBoard(4, 4, " ");
 
 /*
 let str = "     A   B   C  \n   +---+---+---+\n 1 |   |   | O |\n   +---+---+---+\n 2 |   | X |   |\n   +---+---+---+\n 3 |   |   |   |\n   +---+---+---+\n";
 console.log(str);
 */
 //app.getCellsToFlip(board, 1, 4);
-board = app.placeLetters(board, 'O','A2');
-board = app.placeLetters(board, 'X', 'A1');
+board = app.placeLetters(board, 'O','A1', 'C1', 'A2', 'C2');
+board = app.placeLetters(board, 'X', 'B2', 'C2', 'B3', 'C3', 'C4');
 console.log(app.getValidMoves(board, 'X'));
+
+
+//console.log(app.getUpperLeftIndices(board, 2, 1, 3));
 
